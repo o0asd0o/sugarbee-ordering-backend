5f204943852f063bf461b1190404496a9ffe9262
DexmelH <57649261+DexmelH@users.noreply.github.com>
2020-03-29 15:32:53 +0800

    commit

---
+++ routes/login.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var router=express.Router();
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "sugarbee"
+});
+
+router.get("/", function(req, res, next){
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query("SELECT * FROM user", function (err, result, fields) {
+          if (err) throw err;
+          res.send(result);
+        });
+    });
+});
+
+module.exports=router;
