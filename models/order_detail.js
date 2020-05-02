3fac36d85d868cd09d8ea150cb4b4c33f8a84dd6
Jomel Ortega <jomelorskie@gmail.com>
2020-05-02 20:53:39 +0800

    added helpers.js and updated orders API

---
+++ models/order_detail.js
@@ -0,0 +1,31 @@
+const Sequelize = require("sequelize")
+const db = require("../database/db")
+
+module.exports = db.sequelize.define(
+    'order_details',
+    {
+        identifier: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        order_id: {
+            type: Sequelize.INTEGER
+        },
+        item_name: {
+            type: Sequelize.STRING
+        },
+        unit_price: {
+            type: Sequelize.FLOAT
+        },
+        quantity: {
+            type: Sequelize.INTEGER
+        },
+        total_price: {
+            type: Sequelize.FLOAT
+        },
+    },
+    {
+        timestamps: false
+    }
+)
